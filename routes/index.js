d1fc8780b6ebb24606881710d2aed32481cb4964
abir088 <abir.cuet08@gmail.com>
2018-02-01 07:56:02 +0000

    Improved styling login & signup view

--- routes/index.js
+++ routes/index.js
@@ -14,8 +14,8 @@ router.get("/",function(req, res) {
 
 //show register form
 router.get("/register",function(req,res){
-    res.render("register");
-})
+    res.render("register", {page: 'register'});
+});
 //handle Sign Up logic
 router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
@@ -27,14 +27,14 @@ router.post("/register", function(req, res){
         passport.authenticate("local")(req, res, function(){
             req.flash("success","Welcome to YelpCamp "+ user.username+"!");
             res.redirect("/campground");
-        })
-    })
+        });
+    });
 });
 
 //SHOW LOGIN form
 
 router.get("/login",function(req,res){
-    res.render("login");
+    res.render("login", {page: 'login'});
 });
 
 //handling login logic
